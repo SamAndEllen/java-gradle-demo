3f7c4c8672d52da07f7c2b3ffe437564b10d8178
jo-narare <nare6004@gmail.com>
2022-01-17 11:03:58 +0900

    init

---
+++ src/main/java/com/example/javagradledemo/service/UserService.java
@@ -0,0 +1,32 @@
+package com.example.javagradledemo.service;
+
+import com.example.javagradledemo.domain.User;
+import com.example.javagradledemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User> findAllUser() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
